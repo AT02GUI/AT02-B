d195d9fe509b1692a3e4172b5efe0f769ba980f8
Sergio <sergio.landa@fundacion-jala.org>
2017-01-25 17:34:47 +0000

    Model advance search

--- AutomationFrameworkCucumber/mach2/src/test/java/org/funjala/automation/web/mach2/hooks/ModelSearch.java
+++ AutomationFrameworkCucumber/mach2/src/test/java/org/funjala/automation/web/mach2/hooks/ModelSearch.java
@@ -11,15 +11,66 @@ public class ModelSearch {
   private String jobTitle;
   private String location;
   private String lead;
+
+  public String getInternalId() {
+    return internalId;
+  }
+
+  public void setInternalId(String internalId) {
+    this.internalId = internalId;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDepartment() {
+    return department;
+  }
+
+  public void setDepartment(String department) {
+    this.department = department;
+  }
+
+  public String getJobTitle() {
+    return jobTitle;
+  }
+
+  public void setJobTitle(String jobTitle) {
+    this.jobTitle = jobTitle;
+  }
+
+  public String getLocation() {
+    return location;
+  }
+
+  public void setLocation(String location) {
+    this.location = location;
+  }
+
+  public String getLead() {
+    return lead;
+  }
+
+  public void setLead(String lead) {
+    this.lead = lead;
+  }
+
+  public String getManager() {
+    return manager;
+  }
+
+  public void setManager(String manager) {
+    this.manager = manager;
+  }
+
   private String manager;
 
-  public ModelSearch(String internalId, String name, String department, String jobTitle, String location, String lead, String manager) {
-    this.internalId = internalId;
-    this.name = name;
-    this.department = department;
-    this.jobTitle = jobTitle;
-    this.location = location;
-    this.lead = lead;
-    this.manager = manager;
+  public ModelSearch() {
+
   }
 }
